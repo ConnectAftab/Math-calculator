be9db6cd7a965994f37c6ea080b4efbe284d0802
Aftab Ahmad Mir <aftabahmadmir@Aftabs-MacBook-Air.local>
2024-11-03 13:00:47 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,100 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [input,setInput] = useState('');
+const calculatedResult = (input)=>{
+  let result;
+  try{
+    const oprators =["+","-","/","%","*"]
+   let  oprator = null;
+    for (let i = 0; i< input.length; i++){
+      if(oprators.includes(input[i])){
+        oprator= input[i];
+        break;
+      }
+
+    }
+    if (!oprator){
+      setInput(parseFloat(input).toString())
+      return;
+    }
+
+    const [op1,op2] = input.split(oprator).map(parseFloat);
+    switch(oprator){
+      case"+": 
+      result = op1+op2;
+      break;
+      case"-": 
+      result = op1-op2;
+      break;
+      case"*": 
+      result = op1*op2;
+      break;
+      case"/": 
+      result = op1/op2;
+      break;
+      case"%": 
+      result = op2 !== 0 ? op1 % op2 : "Error: Division by zero";
+      break;
+      default:
+         throw new Error("invalid oprator")
+    }
+setInput(result.toString())
+  }
+  catch(error){
+   setInput("Error")
+  }
+}
+  const handleButtonClick = (value)=>{
+    if (value === "C"){
+      setInput('')
+    }else if(value === "<"){
+      setInput(input.slice(0,-1))
+    }else if (value === "="){
+    calculatedResult (input);
+  } 
+    else {
+      setInput((preValue) =>preValue+ value)
+    }
+    
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+
+    <div className='calc'>
+<h1 id='input'>{input}</h1>
+<div>
+  <button onClick={() => handleButtonClick('C')}>C</button>
+  <button onClick={() => handleButtonClick('<')}>&lt;</button>
+  <button onClick={() => handleButtonClick('%')}>%</button>
+  <button onClick={() => handleButtonClick('/')}>/</button>
+</div>
+<div>
+  <button onClick={() => {handleButtonClick('7')}}>7</button>
+  <button onClick={() => {handleButtonClick('8')}}>8</button>
+  <button onClick={() => {handleButtonClick('9')}}>9</button>
+  <button onClick={() => {handleButtonClick('*')}}>*</button>
+</div><div>
+  <button onClick={() => {handleButtonClick('4')}}>4</button>
+  <button onClick={() => {handleButtonClick('5')}}>5</button>
+  <button onClick={() => {handleButtonClick('6')}}>6</button>
+  <button>-</button>
+</div><div>
+  <button onClick={() => {handleButtonClick('1')}}>1</button>
+  <button onClick={() => {handleButtonClick('2')}}>2</button>
+  <button onClick={() => {handleButtonClick('3')}}>3</button>
+  <button onClick={() => {handleButtonClick('+')}}>+</button>
+</div><div>
+  <button onClick={() => {handleButtonClick('0')}}>0</button>
+  <button onClick={() => {handleButtonClick('00')}}>00</button>
+  <button onClick={() => {handleButtonClick('.')}}>.</button>
+  <button onClick={() => {handleButtonClick('=')}}>=</button>
+</div>
     </div>
-  );
+      </div>
+  )
 }
 
-export default App;
+export default App
